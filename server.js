440e3b3a7eb157e8b1dead82fb310acf9fb1b5f5
D-4costa <danielleelisabeth01@gmail.com>
2026-03-09 12:31:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,25 +6,31 @@ import contactsRoutes from './routes/contacts.js';
 import { connectDB } from './models/db.js';
 
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './swagger/swagger.json' assert { type: "json" };
+import fs from 'fs';
+
+const swaggerDocument = JSON.parse(fs.readFileSync('./swagger/swagger.json'));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// API routes
 app.use('/contacts', contactsRoutes);
 
+// Swagger docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Home route
 app.get('/', (req, res) => {
   res.redirect('/api-docs');
 });
 
+// Start server
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Contacts API running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
       console.log(`Swagger docs available at /api-docs`);
     });
   })
